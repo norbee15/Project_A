c5b95a434fadea28831b80bf2fe5494c585103e8
norbee15 <41166622+norbee15@users.noreply.github.com>
2024-06-30 16:29:16 +0200

    Update 1

--- Multiplayer Test/Mutliplayer_test.gd
+++ Multiplayer Test/Mutliplayer_test.gd
@@ -1,47 +1,57 @@
 extends Node2D
 
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-var peer =ENetMultiplayerPeer.new()
-@export var player_scene: PackedScene
-
-
-
 func _on_host_pressed():
+	if multiplayer.multiplayer_peer != null:
+		multiplayer.multiplayer_peer.close_connection()
+		multiplayer.multiplayer_peer = null
+
 	peer.create_server(135)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
+	multiplayer.peer_disconnected.connect(_remove_player)
 	_add_player()
-	
+
+	print("Hosting server on port 135")
 
 func _add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
-	call_deferred("add_child",player)
-	
-	
-	
+	call_deferred("add_child", player)
+	print("Added player with ID:", id)
+
+func _remove_player(id):
+	var player = get_node_or_null(str(id))
+	if player:
+		player.queue_free()
+	print("Removed player with ID:", id)
+
 func _on_join_pressed():
-	peer.create_client("82.150.39.25", 135)
-	multiplayer.multiplayer_peer = peer
-	peer.connect("connection_failed", self, "_on_connection_failed")
-	peer.connect("connection_succeeded", self, "_on_connection_succeeded")
-	peer.connect("server_disconnected", self, "_on_server_disconnected")
+	if multiplayer.multiplayer_peer != null:
+		multiplayer.multiplayer_peer.close_connection()
+		multiplayer.multiplayer_peer = null
+
+	var result = peer.create_client("82.150.39.25", 135)
+	if result == OK:
+		multiplayer.multiplayer_peer = peer
+		multiplayer.connected_to_server.connect(_on_connected_to_server)
+		multiplayer.connection_failed.connect(_on_connection_failed)
+
+		print("Attempting to join server at 82.150.39.25:135")
+	else:
+		print("Failed to create client:", result)
+
+func _on_connected_to_server():
+	print("Successfully connected to server")
+	_add_player(get_tree().get_network_unique_id())
 
 func _on_connection_failed():
-	print("Connection failed!")
-
-func _on_connection_succeeded():
-	print("Connection succeeded!")
-
-func _on_server_disconnected():
-	print("Disconnected from server!")
+	print("Failed to connect to server")
